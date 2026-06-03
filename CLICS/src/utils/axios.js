b8be6b78416b855818360104d7069b9244214453
Fahimullah-67 <179002904+fahimullah-67@users.noreply.github.com.>
2026-06-03 17:11:37 +0500

    Add Railway Url

--- CLICS/src/utils/axios.js
+++ CLICS/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7000/api/v1",
+  baseURL: "https://clics-backend-production.up.railway.app",
 
   //   headers: {
   //     "Content-Type": "application/json",
