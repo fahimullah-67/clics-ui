b5bdd05e78f718d6c630bce839af47856c836cc2
Fahimullah-67 <179002904+fahimullah-67@users.noreply.github.com.>
2026-06-03 17:22:22 +0500

    Update APi

--- CLICS/src/utils/axios.js
+++ CLICS/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://clics-backend-production.up.railway.app",
+  baseURL: "https://clics-backend-production.up.railway.app/api/v1",
 
   //   headers: {
   //     "Content-Type": "application/json",
